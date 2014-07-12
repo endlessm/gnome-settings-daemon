endlessm/gnome-settings-daemon

commit dd2477bf74510237310d41974ba3e342e6c95da1

housekeeping: Don't follow symlinks to subdirectories
Check whether a leaf node is a symlink before processing it, and if it
is, check whether we should delete the link itself.

This works around a possible bug in GIO where GFileEnumerators will get
the filetype of the linked-to item, instead of detecting that it is a
symlink.

https://bugzilla.gnome.org/show_bug.cgi?id=730223

plugins/housekeeping/gsd-disk-space.c
@@ -415,6 +415,44 @@ delete_subdir (GObject      *source,
         delete_data_unref (data);
 }
 
+static void
+delete_subdir_check_symlink (GObject      *source,
+                             GAsyncResult *res,
+                             gpointer      user_data)
+{
+        GFile *file = G_FILE (source);
+        DeleteData *data = user_data;
+        GFileInfo *info;
+        GFileType type;
+
+        info = g_file_query_info_finish (file, res, NULL);
+        if (!info) {
+                delete_data_unref (data);
+                return;
+        }
+
+        type = g_file_info_get_file_type (info);
+        g_object_unref (info);
+
+        if (type == G_FILE_TYPE_DIRECTORY) {
+                g_file_enumerate_children_async (data->file,
+                                                 G_FILE_ATTRIBUTE_STANDARD_NAME ","
+                                                 G_FILE_ATTRIBUTE_STANDARD_TYPE,
+                                                 G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
+                                                 0,
+                                                 data->cancellable,
+                                                 delete_subdir,
+                                                 delete_data_ref (data));
+        } else if (type == G_FILE_TYPE_SYMBOLIC_LINK) {
+                if (should_purge_file (data->file, data->cancellable, data->old)) {
+                        if (!data->dry_run) {
+                                g_file_delete (data->file, data->cancellable, NULL);
+                        }
+                }
+        }
+        delete_data_unref (data);
+}
+
 void
 delete_recursively_by_age (DeleteData *data)
 {
@@ -424,14 +462,13 @@ delete_recursively_by_age (DeleteData *data)
                 return;
         }
 
-        g_file_enumerate_children_async (data->file,
-                                         G_FILE_ATTRIBUTE_STANDARD_NAME ","
-                                         G_FILE_ATTRIBUTE_STANDARD_TYPE,
-                                         G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
-                                         0,
-                                         data->cancellable,
-                                         delete_subdir,
-                                         delete_data_ref (data));
+        g_file_query_info_async (data->file,
+                                 G_FILE_ATTRIBUTE_STANDARD_TYPE,
+                                 G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
+                                 0,
+                                 data->cancellable,
+                                 delete_subdir_check_symlink,
+                                 delete_data_ref (data));
 }
 
 void

commit a31d9cf19feaef03e094f80747fe8ef1dc79d8ce

housekeeping: fix deletion of files in the trash
Fixes a regression from dd2477bf74510237310d41974ba3e342e6c95da1 where
regular files would never get deleted from the trash.

[endlessm/eos-shell#3030]

plugins/housekeeping/gsd-disk-space.c
@@ -435,7 +435,14 @@ delete_subdir_check_symlink (GObject      *source,
         type = g_file_info_get_file_type (info);
         g_object_unref (info);
 
-        if (type == G_FILE_TYPE_DIRECTORY) {
+        if (type == G_FILE_TYPE_SYMBOLIC_LINK) {
+                if (should_purge_file (data->file, data->cancellable, data->old)) {
+                        g_debug ("Purging %s leaf node", data->name);
+                        if (!data->dry_run) {
+                                g_file_delete (data->file, data->cancellable, NULL);
+                        }
+                }
+        } else {
                 g_file_enumerate_children_async (data->file,
                                                  G_FILE_ATTRIBUTE_STANDARD_NAME ","
                                                  G_FILE_ATTRIBUTE_STANDARD_TYPE,
@@ -444,13 +451,6 @@ delete_subdir_check_symlink (GObject      *source,
                                                  data->cancellable,
                                                  delete_subdir,
                                                  delete_data_ref (data));
-        } else if (type == G_FILE_TYPE_SYMBOLIC_LINK) {
-                if (should_purge_file (data->file, data->cancellable, data->old)) {
-                        g_debug ("Purging %s leaf node", data->name);
-                        if (!data->dry_run) {
-                                g_file_delete (data->file, data->cancellable, NULL);
-                        }
-                }
         }
         delete_data_unref (data);
 }