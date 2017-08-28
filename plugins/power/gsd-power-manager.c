87290bbb933aac4ac45a2ecade3f338c6e187dd5
Carlo Caione <carlo@endlessm.com>
2017-08-28 11:03:30 +0200

    power: Monitor org.freedesktop.UPower.KbdBacklight.KbdParamsChanged signal
    With the patches proposed for upower[0], this will make the keyboard
    brightness change buttons woking fine also when the kernel module
    managing the brightness is loaded after upower has already started.
    
    [0] https://bugs.freedesktop.org/show_bug.cgi?id=101244
    
    https://bugzilla.gnome.org/show_bug.cgi?id=783440

--- plugins/power/gsd-power-manager.c
+++ plugins/power/gsd-power-manager.c
@@ -1494,16 +1494,28 @@ upower_kbd_proxy_signal_cb (GDBusProxy  *proxy,
         gint brightness, percentage;
         const gchar *source;
 
-        if (g_strcmp0 (signal_name, "BrightnessChangedWithSource") != 0)
-                return;
+        if (g_strcmp0 (signal_name, "BrightnessChangedWithSource") == 0) {
+	        g_variant_get (parameters, "(i&s)", &brightness, &source);
 
-        g_variant_get (parameters, "(i&s)", &brightness, &source);
+	        /* Ignore changes caused by us calling UPower's SetBrightness method,
+	         * we already call backlight_iface_emit_changed for these after the
+	         * SetBrightness method call completes. */
+	        if (g_strcmp0 (source, "external") == 0)
+	                return;
 
-        /* Ignore changes caused by us calling UPower's SetBrightness method,
-         * we already call backlight_iface_emit_changed for these after the
-         * SetBrightness method call completes. */
-        if (g_strcmp0 (source, "external") == 0)
+        } else if (g_strcmp0 (signal_name, "KbdParamsChanged") == 0) {
+                GVariant *kbd_parameters;
+                GVariant *kbd_br_parameters;
+
+                g_variant_get (parameters, "(o@a{?*})", NULL, &kbd_parameters);
+                kbd_br_parameters = g_variant_lookup_value (kbd_parameters, "brightness", G_VARIANT_TYPE_VARDICT);
+                if (kbd_br_parameters) {
+                        g_variant_lookup (kbd_br_parameters, "value", "i", &brightness);
+                        g_variant_lookup (kbd_br_parameters, "max", "i", &manager->priv->kbd_brightness_max);
+                }
+        } else {
                 return;
+        }
 
         manager->priv->kbd_brightness_now = brightness;
         percentage = ABS_TO_PERCENTAGE (0,
